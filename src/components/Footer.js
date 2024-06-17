834ba429ffe6e79356ed1f85151b14a22f3ec659
Gabrielshow <adefelashogbanmu@gmail.com>
2024-06-18 00:26:42 +0100

    updating information

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -20,7 +20,7 @@ export const Footer = () => {
               <a href="www.facebook.com"><img src={navIcon2} alt="Icon" /></a>
               <a href="www.twitter.com"><img src={navIcon3} alt="Icon" /></a>
             </div>
-            <p>Copyright 2023. All Rights Reserved</p>
+            <p>Copyright 2024. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
